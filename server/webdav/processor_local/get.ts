b97fa8ed754e7b7a0809346d66843091a584d863
shen <bear_shen@live.cn>
2022-11-09 19:41:29 +0800

    f

--- server/webdav/processor_local/get.ts
+++ server/webdav/processor_local/get.ts
@@ -9,7 +9,25 @@ export default async function (req: IncomingMessage, res: ServerResponse) {
     if (!ifExs) return respCode(404, res);
     if (!ifExs.isFile) return respCode(405, res);
     res.statusCode = 206;
-    const rs = fp.get(relPath, 0, ifExs.size);
+    //
+    let bufFrom = 0;
+    let bufTo = ifExs.size;
+    if (req.headers.range) {
+        const rngArr = req.headers.range.split('=');
+        if (rngArr.length > 1) {
+            const byteArr = rngArr[1].split('-');
+            //不知道为什么会有from=to的请求...
+            if (byteArr.length === 2 && byteArr[0] !== byteArr[1]) {
+                bufFrom = Number.parseInt(byteArr[0]);
+                bufTo = Number.parseInt(byteArr[1]);
+                if (bufTo > ifExs.size) {
+                    bufTo = ifExs.size;
+                }
+            }
+        }
+    }
+    //
+    const rs = fp.get(relPath, bufFrom, bufTo);
     await setResponseFile(rs, res);
     return;
 }
