80ab9e291b3d2ef1286f62fc743a8edb29a57290
shen <bear_shen@live.cn>
2023-04-20 22:30:51 +0800

    f

--- resource/matchDir.php
+++ resource/matchDir.php
@@ -1,15 +1,151 @@
 <?php
 require_once 'bootloader.php';
 require_once 'GenFunc.php';
-$sourceDir = $argv[0];
-$targetDir = $argv[1];
-$fls       = scandir($sourceDir);
-//$meta      = new folderMeta($sourceDir . 'orph');
-//exit();
+$sourceDir = 'F:\\newdown\\erohun\\';
+$tmpDir    = 'F:\\newdown\\erohun_tmp\\';
+$targetDir = 'F:\\newdown\\erohun_proc\\';
+//------------------------------------------
+//获取已分组数据
+$tMetaLs = [];
+$tls     = scandir($targetDir);
+foreach ($tls as $fl) {
+    if ($fl == '.' || $fl == '..') continue;
+    $meta = new groupMeta($targetDir . $fl);
+//    var_dump($meta);
+    $stls  = scandir($targetDir . $fl);
+    $keyLs = [];
+    foreach ($stls as $stl) {
+        if ($stl == '.' || $stl == '..') continue;
+        if (!is_dir($targetDir . $fl . '\\' . $stl)) continue;
+        $sMeta = new folderMeta($targetDir . $fl . '\\' . $stl);
+        if ($meta->isParody) {
+            if ($sMeta->parody) $keyLs[] = $sMeta->parody;
+        }
+        if ($meta->isAuthor) {
+            if ($sMeta->group) $keyLs[] = $sMeta->group;
+            if ($sMeta->author) $keyLs[] = $sMeta->author;
+        }
+    }
+    $keyLs       = array_filter($keyLs, function ($val) {
+        $fKey = [
+            '汉化', '翻译', '漢化', '中国', '翻訳', 'chinese', 'english', '翻訳',
+            "r18", "r-18", "高解像度", "psd", "补档",
+            "えっちビデオ", "どえっち動画", "序章", "胡桃", "どえっち特別版", "無料版！",
+            "※r-18注意※", "※微エロ注意※", "同人誌", "r-18", "まとめ1", "※エロ注意※", "まとめ2", "リクエスト", "全体公開", "食べ放題プラン限定", "ちょいエロ", "乳首あり注意", "まとめ", "新春リク", "skeb", "みんなで自己紹介",
+            "差分",
+            "表情", "セリフ", "再販",
+        ];
+        foreach ($fKey as $key) {
+            if (stripos($val, $key)) return false;
+        }
+        return true;
+    });
+    $meta->keyLs = array_keys(array_flip($keyLs));
+//    break;
+    $tMetaLs[] = $meta;
+}
+file_put_contents(__DIR__ . '/meta.dir.json', json_encode($tMetaLs, JSON_UNESCAPED_UNICODE));
+//------------------------------------------
+//获取分组数据
+$fMetaLs = [];
+$fls     = scandir($sourceDir);
 foreach ($fls as $fl) {
     if ($fl == '.' || $fl == '..') continue;
     $meta = new folderMeta($sourceDir . $fl);
+//    if (empty($meta->name))
+    $fMetaLs[] = $meta;
 //    break;
+}
+file_put_contents(__DIR__ . '/meta.src.json', json_encode($fMetaLs, JSON_UNESCAPED_UNICODE));
+
+foreach ($fMetaLs as $i => $f) {
+    $toDir = '';
+    if (empty($toDir)) {
+        foreach ($tMetaLs as $t) {
+            if (!$t->isAuthor) continue;
+            foreach ($t->keyLs as $key) {
+                if ($f->author == $key) $toDir = '作品合集 ' . $t->name;
+                if ($f->group == $key) $toDir = '作品合集 ' . $t->name;
+            }
+        }
+    }
+    if (empty($toDir)) {
+        foreach ($tMetaLs as $t) {
+            if (!$t->isParody) continue;
+            foreach ($t->keyLs as $key) {
+                if ($f->parody == $key) $toDir = '合集 ' . $t->name;
+            }
+        }
+    }
+    if (empty($toDir)) {
+        continue;
+//        if (!empty($f->author)) $toDir = 'n 作品合集 ' . $f->author;
+//        else if (!empty($f->group)) $toDir = 'n 作品合集 ' . $f->group;
+//        else $toDir = 'unknown';
+    }
+    $toDirPath = $tmpDir . $toDir;
+    if (!file_exists($toDirPath))
+        @mkdir($toDirPath, 0777, true);
+    $mvTo = $toDirPath . '\\' . $f->name;
+    @rename($f->path, $mvTo);
+    unset($fMetaLs[$i]);
+}
+//有多个项目的时候才创建分组
+$fMetaLs        = array_values($fMetaLs);
+$authCountMap   = [];
+$parodyCountMap = [];
+foreach ($fMetaLs as $i => $f) {
+    /** @var $f folderMeta */
+    $k = $f->author;
+    if (!empty($k)) {
+        if (empty($authCountMap[$k])) $authCountMap[$k] = 0;
+        $authCountMap[$k] += 1;
+    }
+    $k = $f->group;
+    if (!empty($k)) {
+        if (empty($authCountMap[$k])) $authCountMap[$k] = 0;
+        $authCountMap[$k] += 1;
+    }
+    $k = $f->parody;
+    if (!empty($k)) {
+        if (empty($parodyCountMap[$k])) $parodyCountMap[$k] = 0;
+        $parodyCountMap[$k] += 1;
+    }
+}
+foreach ($authCountMap as $author => $count) {
+    if ($count < 2) continue;
+    $toDirPath = $tmpDir . 'n作品合集 ' . $author;
+    if (!file_exists($toDirPath))
+        @mkdir($toDirPath, 0777, true);
+    $fMetaLs = array_values($fMetaLs);
+    foreach ($fMetaLs as $i => $f) {
+        /** @var $f folderMeta */
+        if ($f->author == $author) {
+            $mvTo = $toDirPath . '\\' . $f->name;
+            @rename($f->path, $mvTo);
+            unset($fMetaLs[$i]);
+        }
+        if ($f->group == $author) {
+            $mvTo = $toDirPath . '\\' . $f->name;
+            @rename($f->path, $mvTo);
+            unset($fMetaLs[$i]);
+        }
+    }
+}
+foreach ($parodyCountMap as $parody => $count) {
+    if ($count < 2) continue;
+    $toDirPath = $tmpDir . 'n合集 ' . $parody;
+    if (!file_exists($toDirPath))
+        @mkdir($toDirPath, 0777, true);
+    $fMetaLs = array_values($fMetaLs);
+    foreach ($fMetaLs as $i => $f) {
+        /** @var $f folderMeta */
+        if ($f->parody == $parody) {
+            $mvTo = $toDirPath . '\\' . $f->name;
+            @rename($f->path, $mvTo);
+            unset($fMetaLs[$i]);
+        }
+    }
 }
 
 //similar_text()
@@ -17,13 +153,28 @@ class groupMeta {
     public $path;
     public $dirPath;
     public $name;
+    public $isManga;
     public $isParody;
     public $isAuthor;
+    public $keyLs = [];
 
     public function __construct($fullPath) {
         $this->path    = $fullPath;
         $this->dirPath = dirname($fullPath);
-//        $this->name    = basename($fullPath);
+        $baseName      = basename($fullPath);
+//        var_dump($baseName);
+        if (stripos($baseName, '作品合集') !== false) {
+            $this->name     = mb_substr($baseName, 5);
+            $this->isAuthor = true;
+        } else if (stripos($baseName, '杂志合集') !== false) {
+            $this->name    = mb_substr($baseName, 5);
+            $this->isManga = true;
+        } else if (stripos($baseName, '合集') !== false) {
+            $this->name     = mb_substr($baseName, 3);
+            $this->isParody = true;
+        } else {
+            echo implode("\t", [$fullPath]), "\r\n";
+        }
     }
 }
 
@@ -53,6 +204,10 @@ class folderMeta {
         $l           = '\(（';
         $matchParody = [];
         preg_match("/.*[$l]([^$r$l]+)[$r]*/iu", $this->name, $matchParody);
+        if (empty($matchParody)) {
+            echo implode("\t", [$this->name]), "\r\n";
+            return;
+        }
         $this->parody = mb_trim($matchParody[1]);
         if (empty($this->parody)) {
             echo implode("\t", [$this->name]), "\r\n";
