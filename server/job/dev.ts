61172be4ff9b3ecff350f19c524f91d32fe3b89e
shen <bear_shen@live.cn>
2023-12-07 09:41:36 +0800

    ff bench dev

--- server/job/dev.ts
+++ server/job/dev.ts
@@ -1,8 +1,56 @@
+import util from "util";
+
+console.info('job dev init');
+//multitask bench
+const {
+    Worker, isMainThread, parentPort, workerData, threadId,
+} = require('node:worker_threads');
+const exec = util.promisify(require('child_process').exec);
+if (isMainThread) {
+    const workerThreads = 4;
+    const exitSleep = 5 * 1000;
+    const workerMap: Map<number, Worker> = new Map();
+
+    for (let i1 = 0; i1 < workerThreads; i1++) {
+        buildThread(i1);
+    }
+
+    function buildThread(index: number) {
+        const worker = new Worker(__filename, {
+            workerData: [workerThreads, index,],
+        });
+        console.info('build thread index:', index, ' , id:', worker.threadId);
+        worker.on('online', () => {
+            console.info('main:online:', index, ' , id:', worker.threadId);
+        });
+        worker.on('error', (value: any) => {
+            console.info('main:error:', index, ' , id:', worker.threadId, value);
+        });
+        worker.on('exit', () => {
+            console.info('main:exit', index);
+            //exit之后已经取不到threadId了，不清楚有没有例外情况
+            // if (worker.threadId)
+            workerMap.delete(index);
+            setTimeout(buildThread.bind(this, index), exitSleep);
+        });
+        workerMap.set(index, worker);
+    }
+} else {
+    console.info('sub:construct');
+    const cmd = `ffmpeg.exe -v quiet -hide_banner 
+-i /home/vSample/i.mkv
+-c:v av1_nvenc -profile:v main10 -preset slow -tune hq -qp 40 -bf 3 -pix_fmt p010le
+-c:a aac -q:a 1.3
+/home/vSample/o.${threadId}.mkv
+`.replace(/[\r\n]+/ig,' ');
+
+}
+/*
+//generic
 // import fs from "fs";
 import {loadConfig} from "../ServerConfig";
 import ExtJob from "./processor/ExtJob";
 
-console.info('job dev init');
 
 loadConfig().then(async () => {
     console.info('start');
@@ -27,4 +75,4 @@ loadConfig().then(async () => {
     //     }
     // );
     console.info('complete');
-});
+});*/
