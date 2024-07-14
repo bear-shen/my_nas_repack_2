4d0aa22f0a46a365ec84bb9a0f52d322eecaf3e8
shen <bear_shen@live.cn>
2024-07-14 23:47:15 +0800

    一部分文件的扩展名不正确，兼容一下

--- resource/scanFileEOF.php
+++ resource/scanFileEOF.php
@@ -4,10 +4,10 @@ require_once 'lib_php/GenFunc.php';
 //
 $sourceDir = $argv[1];
 $eofDef    = [
-    'jpg'  => [['ff', 'd9'],],
-    'jpeg' => [['ff', 'd9'],],
-    'png'  => [['60', '82'],],
-    'webp' => [['00'],],
+    'jpg'  => [['ff', 'd9'],['0d', '0a'],['60', '82'],],
+    'jpeg' => [['ff', 'd9'],['0d', '0a'],['60', '82'],],
+    'png'  => [['60', '82'],['ff', 'd9'],['0d', '0a'],],
+    //    'webp' => [['00'],],
 ];
 //var_dump($sourceDir);
 //var_dump(__FILE__);
@@ -24,10 +24,11 @@ foreach ($sourceLs as $sourceFile) {
     if (empty($eofDef[$ext])) continue;
     $eofLs = $eofDef[$ext];
     //
-    $ch    = fopen($sourceFile, 'r');
-    $size  = filesize($sourceFile);
-    $match = true;
+    $ch      = fopen($sourceFile, 'r');
+    $size    = filesize($sourceFile);
+    $matchLs = [];
     foreach ($eofLs as $eof) {
+        $match = true;
         if ($size < sizeof($eof)) {
             $match = false;
             break;
@@ -38,10 +39,17 @@ foreach ($sourceLs as $sourceFile) {
             fseek($ch, -1 * ($indR + 1), SEEK_END);
             $r = fread($ch, 1);
             if ($char != bin2hex($r)) $match = false;
-            break 2;
+            break;
         }
+        $matchLs[] = $match;
     }
-    if (!$match) {
+    $hasMatch = false;
+    foreach ($matchLs as $match) {
+        if (!$match) continue;
+        $hasMatch = true;
+        break;
+    }
+    if (!$hasMatch) {
         fseek($ch, -10, SEEK_END);
         $r = fread($ch, 10);
         $s = 'EOF mismatch:' . $sourceFile . "\t" . bin2hex($r) . "\r\n";
