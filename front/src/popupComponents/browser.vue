c103275e8fd6d6e0d5b2786c6c1124651c8fcd6a
shen <bear_shen@live.cn>
2024-07-29 17:17:19 +0800

    移动端屏蔽pdf

--- front/src/popupComponents/browser.vue
+++ front/src/popupComponents/browser.vue
@@ -26,14 +26,20 @@ const props = defineProps<{
   modalData: ModalStruct;
 }>();
 const isMobile = window.navigator.userAgent.toLowerCase().indexOf('mobile') !== -1;
-const regComponentLs = {
+const regComponentLs: { [key: string]: any } = isMobile ? {
+  audio: browserAudioVue,
+  video: browserVideoVue,
+  image: browserImageVue,
+  text: browserTextVue,
+  base: browserBaseVue,
+} : {
   audio: browserAudioVue,
   video: browserVideoVue,
   image: browserImageVue,
   pdf: browserPDFVue,
   text: browserTextVue,
   base: browserBaseVue,
-} as { [key: string]: any };
+};
 
 const eventStore = useEventStore();
 const localConfigure = useLocalConfigureStore();
@@ -483,16 +489,16 @@ function setRater(rateVal: string) {
       <!--      </div>-->
       <div class="btnContainer">
         <select class="sysIcon" v-model="rateVal" @change="setRater(rateVal)">
-          <option v-for="(type,key) in Config.rate" :value="key" v-html="type"></option>
+          <option v-for="(type,key) in Config.rate" :value="key" v-html="type" :key="'BROWSER_RATE_'+key"></option>
         </select>
         <select v-model="filterVal" @change="setFilter(filterVal)">
-          <option v-for="(fileType, key) in Config.fileType" :value="fileType">
+          <option v-for="(fileType, key) in Config.fileType" :value="fileType" :key="'BROWSER_TYPE_'+key">
             {{ fileType }}
           </option>
         </select>
         <br>
         <select v-model="sortVal" @change="setSort($event)">
-          <option v-for="(sortItem, key) in Config.sort" :value="key">
+          <option v-for="(sortItem, key) in Config.sort" :value="key" :key="'BROWSER_SORT_'+key">
             {{ sortItem }}
           </option>
         </select>
