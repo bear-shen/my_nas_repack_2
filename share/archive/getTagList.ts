ae12f8b2d01a6da8d99640d3e11e089e32c4bab9
shen <bear_shen@live.cn>
2025-03-01 14:14:39 +0800

    k share/archive

--- share/archive/getTagList.ts
+++
@@ -1,8 +0,0 @@
-export default [
-  { id: 1, title: 'tag title1', id_group: 1, title_group: 'tag group1', },
-  { id: 2, title: 'tag title2', id_group: 1, title_group: 'tag group1', },
-  { id: 3, title: 'tag title3', id_group: 1, title_group: 'tag group1', },
-  { id: 1, title: 'tag title2-1', id_group: 2, title_group: 'tag group2', },
-  { id: 2, title: 'tag title2-2', id_group: 2, title_group: 'tag group2', },
-];
-
