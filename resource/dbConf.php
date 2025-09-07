4383defff9cc70e57652d227e9ebd8f9b94c506f
shen <bear_shen@live.cn>
2025-09-07 18:30:00 +0800

    support _info.json

--- resource/dbConf.php
+++ resource/dbConf.php
@@ -1,9 +1,10 @@
 <?php
 global $dbConf;
-$dbConf          = [
-    'host' => '127.0.0.1',
-    'port' => '5432',
-    'db'   => 'toshokan',
-    'name' => 'postgres',
-    'pass' => 'postgres',
+$dbConf = [
+//    'host' => '127.0.0.1',
+'host' => '172.16.1.210',
+'port' => '5432',
+'db'   => 'toshokan',
+'name' => 'postgres',
+'pass' => 'postgres',
 ];
